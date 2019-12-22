6eb9e8dad711de662c45af32a7757ce1c1a5b28b
Charles Pereira <charlesti.ptu@gmail.com>
2019-12-21 23:24:50 -0300

    Conectando e buscando Rest/Full API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from './services/api';
 
-import "./stylesGlobal.css";
+import './stylesGlobal.css';
 
 import Header from './components/Header';
+import Main from './pages/main';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
