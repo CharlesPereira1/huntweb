23d6d63ac868e2573de26516d8752961b2e52575
Charles Pereira <charlesti.ptu@gmail.com>
2019-12-21 23:10:30 -0300

    Criando Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import "./stylesGlobal.css";
+
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
     </div>
   );
 }
